5819dcf4af76bced562bbb8e68fe7e4e58b941e4
sujon13 <arifurrahmansujon27@gmail.com>
2020-05-23 15:06:29 +0600

    Added files and changes required for deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn corona_update.wsgi --log-file -
--- corona_update/settings.py
+++ corona_update/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
